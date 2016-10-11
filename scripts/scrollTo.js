9a09c055af6d59489b62baa5f8bfc22eda61e940
ichicoro <smartex999@gmail.com>
2016-10-11 17:59:13 +0200

    Changed the height at which the scrollTo event fires

--- scripts/scrollTo.js
+++ scripts/scrollTo.js
@@ -1,5 +1,5 @@
 $(document).on('scroll', function() {
-   	if($(this).scrollTop()>=$('#about').position().top){
+   	if($(this).scrollTop() + parseInt(51) >=$('#about').position().top){
        	document.getElementById("header").className = "semi";
    	} else {
        	document.getElementById("header").className = "transparent";
