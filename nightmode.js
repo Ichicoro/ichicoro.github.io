30b88d37a0fb000df32f961198ff5be7f377a82c
Ichicoro <smartex999@gmail.com>
2017-12-27 11:52:35 +0100

    Fix the website's responsiveness

--- nightmode.js
+++ nightmode.js
@@ -2,11 +2,20 @@ var isDark = false;
 
 const toggleTheme = () => {
     const isCurrentlyLight = (isDark ^= 1);
-	document.body.className = isCurrentlyLight ? "darktheme" : "lighttheme";
-    var classn = isCurrentlyLight ? "darktheme" : "lighttheme";
+    var _hexcolor;
+    var _csscolor;
+    if (isCurrentlyLight) {
+    	_hexcolor = "#333333";
+    	_csscolor = "darktheme";
+    } else {
+    	_hexcolor = "#EEEEEE";
+    	_csscolor = "lighttheme";
+    }
+	document.body.className = _csscolor;
+    document.querySelector('meta[name="theme-color"]').setAttribute("content", _hexcolor);
     const list = document.getElementsByTagName("path");
     for (var i = 0; i < list.length; i++) {
-        list[i].className = classn;
+        list[i].className = _csscolor;
         console.log(list[i]);
     };
 }
