851a0f88a93f9c35c1d3a0d27d522c5b42528b2c
Ichicoro <smartex999@gmail.com>
2018-01-09 12:33:09 +0100

    Start working on themes

---
+++ themes.js
@@ -0,0 +1,10 @@
+class Colorscheme {
+	constructor(bgcolor, fgcolor) {
+		this.bgcolor = bgcolor;
+		this.fgcolor = fgcolor;
+	}
+	//const bgcolor;
+	//const fgcolor;
+	// const bgimg; // TODO: IMPLEMENT BACKGROUNDS
+}
+
